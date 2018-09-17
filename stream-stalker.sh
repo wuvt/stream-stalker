b3d1b0cec80a5d6d3b16b5dcfa62ea20b870074a
Dustin Smith <dws@vt.edu>
2018-09-17 15:16:13 -0400

    Grab listeners from icecast because website API isn't live

--- stream-stalker.sh
+++ stream-stalker.sh
@@ -2,7 +2,8 @@
 
 OUT=stream-stalker.csv 
 ZZZ=60
-URL="https://www.wuvt.vt.edu/playlists/latest_track"
+TRACK_INFO="https://www.wuvt.vt.edu/playlists/latest_track"
+LISTENER_INFO="http://stream.wuvt.vt.edu/status-json.xsl"
 
 # Check if csv file already exists, and if not, create it
 if [ ! -f $OUT ]; then
@@ -13,13 +14,14 @@ fi
 # Start looping
 while true
 do
-    curl -s "${URL}" -H "Accept: application/json" -o stream.tmp
+    curl -s "${TRACK_INFO}" -H "Accept: application/json" -o stream.tmp
+    curl -s "${LISTENER_INFO}" -o listener.tmp
     TIMESTAMP=$(date +"%Y/%m/%d %H:%M:%S")
     DJ=$(jq -r '.dj' stream.tmp)
     ARTIST=$(jq -r '.artist' stream.tmp)
     TITLE=$(jq -r '.title' stream.tmp)
     ALBUM=$(jq -r '.album' stream.tmp)
-    LISTENERS=$(jq -r '.listeners' stream.tmp)
+    LISTENERS=$(jq '.icestats.source | .[] | .listeners' listener.tmp | awk '{s+=$1} END {print s}')
     LINE=$TIMESTAMP,\"$DJ\",\"$ARTIST\",\"$TITLE\",\"$ALBUM\",$LISTENERS
     echo $LINE
     echo $LINE >> $OUT
